694a07766bc105926f31c8cc38244423b0383009
Nguyenha <you@example.com>
2019-08-27 16:16:12 +0700

    thuc hanh

---
+++ ham.ts
@@ -0,0 +1,19 @@
+function square(num: number): number {
+    return num * num;
+}
+
+console.log(square(5));
+// Output: 25
+
+// Function expression
+
+// Higher-order function
+function add(a: number): Function {
+    return function(b: number): number {
+        return a + b;
+    }
+}
+
+const addWith5 = add(5);
+console.log(addWith5(3));
+console.log(addWith5(15));
